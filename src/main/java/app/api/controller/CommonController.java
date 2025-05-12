9c35c2b4e42d7a3413b4571f7abf7d6465bdb943
kimrma370 <kimrma370@gmail.com>
2025-05-12 16:58:36 +0900

    add feature : getEmps - EmpApiController

--- src/main/java/app/api/controller/CommonController.java
+++ src/main/java/app/api/controller/CommonController.java
@@ -9,9 +9,14 @@ public class CommonController {
 	public String infoCloud1() {
 		return "KJS1";
 	}
+	
+	@GetMapping("/KJY")
+	public String infoKJY() {
+		return "KJY";
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+	}
 
 }
--- src/main/java/app/api/controller/EmpApiController.java
+++ src/main/java/app/api/controller/EmpApiController.java
@@ -1,5 +1,7 @@
 package app.api.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +20,11 @@ public class EmpApiController {
 	@GetMapping("/api/emp-test")
 	public String empTest() {
 		return "emp api controller" ;
+	}
+	
+	@GetMapping("/api/emps")
+	public List<Emp> getEmps() {
+		return empRepository.findAll();
 	}
 
     	@Autowired
