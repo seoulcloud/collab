c7847b24e2e7a56abd4d55cbcdcf4fdc79e24a8c
wonshee <shee2.won@gmail.com>
2025-05-12 16:49:18 +0900

    wonshee update1

--- src/main/java/app/api/controller/EmpApiController.java
+++ src/main/java/app/api/controller/EmpApiController.java
@@ -1,6 +1,7 @@
 package app.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,7 +9,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 
 import app.entity.Emp;
 import app.repository.EmpRepository;
@@ -28,6 +28,15 @@ public class EmpApiController {
 	    public Emp createEmp(@RequestBody Emp emp) {
 	        return empRepository.save(emp);
 	    }
+	    
+	    // 특정 사원 조회 - 원서희
+	    @GetMapping("/{empno}")
+	    public ResponseEntity<Emp> getEmpinfo(@PathVariable("empno") Integer empno) {
+	        return empRepository.findById(empno)
+	                .map(ResponseEntity::ok)
+	                .orElse(ResponseEntity.notFound().build());
+	    }
+	    
 	    @DeleteMapping("/{id}")
 	    public void deleteEmp(@PathVariable Long id) {
 	        empRepository.deleteById(id);
