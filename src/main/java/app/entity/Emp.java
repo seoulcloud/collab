6444e3fda757a2545eef2ac1f9cd7e555550cde6
sinascode <sina@dankook.ac.kr>
2025-05-12 17:29:31 +0900

    add feature : putEmp - EmpApiController

--- src/main/java/app/entity/Emp.java
+++ src/main/java/app/entity/Emp.java
@@ -10,10 +10,12 @@ import jakarta.persistence.ManyToOne;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
 
 @NoArgsConstructor
 @Getter
+@Setter
 @ToString
 @Builder
 @Entity
