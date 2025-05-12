bad712f36d0965cd1a8dd28545b855fcd7b1ccb6
seoulcloud <integrall92@gmail.com>
2025-05-12 14:41:20 +0900

    add feature : intoCloud1 - CommonController

---
+++ src/main/java/app/api/controller/CommonController.java
@@ -0,0 +1,17 @@
+package app.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class CommonController {
+	
+	@GetMapping("/KJS1")
+	public String infoCloud1() {
+		return "KJS1";
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
